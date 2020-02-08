2b975490536747e5da6de8e90a844db9bcedff78
professorfun <j22421333@gmail.com>
2020-02-09 00:20:48 +0800

    push

--- js/time_count.js
+++ js/time_count.js
@@ -1,67 +1,91 @@
-var second=0;
-var minute=0;
-var hour=0;
-window.setTimeout("interval();",1000);
-function interval()
-{
+var second = 0;
+var minute = 0;
+var hour = 0;
+window.setTimeout("interval();", 1000);
+function interval() {
 	second++;
-	if(second==60)
-	{
-	second=0;minute+=1;
-	}
-	if(minute==60)
-	{
-	minute=0;hour+=1;
-    }
-	window.setTimeout("interval();",1000);
+	window.setTimeout("interval();", 1000);
 }
-	window.onload = function check_usr_ip() {
-		console.log("Client IP Address ：" + VIH_HostIP);
-		console.log(returnCitySN["cip"] + ',' + returnCitySN["cname"]);
-	}
+window.onload = function check_usr_ip() {
+	console.log("Client IP Address ：" + VIH_HostIP);
+	console.log(returnCitySN["cip"] + ',' + returnCitySN["cname"]);
+}
 
-	var last_obj_click=null;
-	function send_message(e) {
-		var obj_ID = e.innerHTML;
-		last_obj_click = localStorage.last_onclick;
-		console.log("user IP Address ：" + VIH_HostIP);
-		console.log("user last onclick:"+last_obj_click);
-		console.log("user onclick : "+obj_ID);
-		localStorage.last_onclick=obj_ID;
-	}
+var last_obj_click = null;
+function send_message(e) {
+	var obj_ID = e.innerHTML;
+	last_obj_click = localStorage.last_onclick;
+	console.log("user IP Address ：" + VIH_HostIP);
+	console.log("user last onclick:" + last_obj_click);
+	console.log("user onclick : " + obj_ID);
+	localStorage.last_onclick = obj_ID;
+}
 
-	/*window.addEventListener('mousemove', (event) => {
-		
-		console.log(`${event.pageX},${event.pageY}`);
-	})*/
+/*window.addEventListener('mousemove', (event) => {
+	
+	console.log(`${event.pageX},${event.pageY}`);
+})*/
 
-	window.addEventListener('click', (event) => {
-		console.log("user click :"+`${event.pageX},${event.pageY}`);
-	})
+window.addEventListener('click', (event) => {
+	console.log("user click :" + `${event.pageX},${event.pageY}`);
+})
 
-	function log_out() {
-		//localStorage.clear();
-	}
+function log_out() {
+	localStorage.clear();
+}
 
-	window.onbeforeunload = function(){
-		var page_name=document.getElementById("title").innerHTML;
-		console.log(page_name+"停留時間"+hour+"時"+minute+"分"+second+"秒");
+window.onbeforeunload = function () {
+	var page_name = document.getElementById("title").innerHTML;
+	console.log(page_name + "停留時間" + second + "秒");
+}
+function purchase() {
+	var item = document.getElementById("title").innerHTML;
+	console.log(user_name + " buy " + item);
+}
+
+var user = localStorage.user;
+function shoppingcar() {
+	var msg = "您真的確定加入購物車嗎？";
+	var item = document.getElementById("title").innerHTML;
+	if (confirm(msg) == true) {
+		var obj = localStorage.obj_list;
+		obj += ',' + item;
+		var tmp = obj.split(',');
+		var tmp_length = tmp.length;
+		if (tmp[0] == "undefined") {
+			tmp.shift(tmp[0]);
 		}
-	function purchase(){
-		var item = document.getElementById("title").innerHTML;
-		console.log(user_name+" buy "+item);
-	}
+		localStorage.obj_list = tmp;
+		for (i = 0; i < tmp_length; i++) {
+			if (tmp[i] == item) {
 
-	var user = localStorage.user
-	shop = {};
-	shop.shoppingcar_list = [];
-	shop.shoppingcar_list.push(user);
-	localStorage.setItem("totalshoppingcar", JSON.stringify(shop));
-	function shoppingcar(){
-		var item = document.getElementById("title").innerHTML;
-		shop = JSON.parse(localStorage.getItem("totalshoppingcar"));
-		shop.shoppingcar_list.push(item);
-		localStorage.setItem("totalshoppingcar", JSON.stringify(shop));
-		console.log(shop.shoppingcar_list);
-		console.log("local:"+localStorage.getItem("totalshoppingcar"));
+			} else {
+				//localStorage.obj_list = tmp;
+			}
+		}
+		console.log("local = " + localStorage.obj_list);
 	}
+}
+
+function append_table(data) {
+	$('.tab').append(
+		$('<tr class="append-data">').append(
+
+			$('<td class="title">').append(
+				$('<p>',
+					{ text: data[1] })
+			),
+
+			$('<td class="tag">').append(
+				$('<p>',
+					{ text: data[2] })
+			),
+
+			$('<td colspan="2" class="edit">').append(
+				$('<input type="button" onclick="Delete_Id(this)" value="刪除" float: right">'),
+				$('<p>', { text: data[0] })
+			)
+		)
+	);
+	$("td.edit").children("p").css("display", "none");
+}

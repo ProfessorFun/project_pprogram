673db93c063270055c34f16ea70743cf5f344043
professorfun <j22421333@gmail.com>
2020-02-09 18:26:06 +0800

    push

--- js/time_count.js
+++ js/time_count.js
@@ -23,43 +23,93 @@ window.addEventListener('click', (event) => {//click event listener
 	console.log("user click :" + `${event.pageX},${event.pageY}`);
 })
 
+
 function log_out() {
-	localStorage.clear();
+	localStorage.removeItem("last_onclick");
+	localStorage.removeItem("password");
+	localStorage.removeItem("obj_list");
+	localStorage.removeItem("user");
+	localStorage.removeItem("user click");
 }
 
 var page_name = document.getElementById("title").innerHTML;
 window.onbeforeunload = function () {//send how many seconds you stay in this page
-	$.ajax({
-		url: 'https://my-database-267023.appspot.com/test',
-		type: "POST",
-		dataType: 'json',
-		data: JSON.stringify({ "username": user, "data2": second }),
+	var page_split = page_name.split('-');
+	var page_name_length = page_name.length;
+	if (page_name_length == 2) {
+		var big_category = page_split[0];
+		var sub_category = page_split[1];
+		$.ajax({
+			url: 'http://my-database-267023.appspot.com/buy_record',
+			type: "POST",
+			dataType: 'json',
+			data: JSON.stringify({
+				"userid": user,
+				"category": big_category,
+				"sub_category": sub_category,
+				"item_name": null,
+				"duration": second
+			}),
 
-		success: function (result) {
-			console.log(page_name + "停留時間" + second + "秒");
-		},
-		error: function () {
-			console.log(second + "send was fail");
-		}
-	});
+			success: function (result) {
+				console.log(page_name + "停留時間" + second + "秒");
+			},
+			error: function () {
+				console.log(second + "send was fail");
+			}
+		});
+	} else if (page_name_length == 3) {
+		var big_category = page_split[0];
+		var sub_category = page_split[1];
+		var product_name = page_split[2];
+		$.ajax({
+			url: 'http://my-database-267023.appspot.com/buy_record',
+			type: "POST",
+			dataType: 'json',
+			data: JSON.stringify({
+				"userid": user,
+				"category": big_category,
+				"sub_category": sub_category,
+				"item_name": product_name,
+				"duration": second
+			}),
+
+			success: function (result) {
+				console.log(page_name + "停留時間" + second + "秒");
+			},
+			error: function () {
+				console.log(second + "send was fail");
+			}
+		});
+	}
 }
+
+
 
 var item = document.getElementById("title").innerHTML;
 function purchase() {
-	console.log(item + "buy was fail");
+	var page_split = item.split('-');
+	var item_id = page_split[2];
+	console.log("userid: " + user);
+	console.log("userid: " + item_id);
+	/*
 	$.ajax({
-		url: 'https://my-database-267023.appspot.com/test',
+		url: 'http://my-database-267023.appspot.com/buy_record',
 		type: "POST",
 		dataType: 'json',
-		data: JSON.stringify({ "username": user, "data2": item }),
+		data: JSON.stringify({
+			"order_number":order_number,
+			"uesr_id": user,
+			"item_id": item_id
+		}),
 
 		success: function (result) {
-			console.log(user_name + " buy " + item);
+			console.log(user + " buy " + item_id);
 		},
 		error: function () {
 			console.log(item + "buy was fail");
 		}
-	});
+	});*/
 }
 
 var user = localStorage.user;
